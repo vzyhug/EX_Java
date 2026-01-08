d5afb05be768891d7567aecd63daa4121a12877b
Hung <duyhung325tt@gmail.com>
2026-01-08 21:22:39 +0700

    toi uu bai 1.9

--- src/bai1_9.java
+++ src/bai1_9.java
@@ -2,21 +2,21 @@ import java.util.Scanner;
 public class bai1_9 {
     static Scanner sc = new Scanner(System.in);
     public static int paymentBill(int unit){
-        int money = 0;
+
         if(0<=unit && unit<=50){
-            return money += 1480*unit;
+            return 1480*unit;
         }
         else if(unit <= 100){
-            return money += 1533*(unit-50) + paymentBill(50);
+            return 1533*(unit-50) + paymentBill(50);
         }
         else if(unit <= 200){
-            return money += 1786*(unit-100) + paymentBill(100);
+            return 1786*(unit-100) + paymentBill(100);
         }
         else if(unit <= 300){
-            return money += 2242*(unit-200) + paymentBill(200);
+            return 2242*(unit-200) + paymentBill(200);
         }
         else{
-            return money += 2503*(unit-300) + paymentBill(300);
+            return 2503*(unit-300) + paymentBill(300);
         }
     }
     public static void main(String[] args) {
