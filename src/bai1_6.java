2a22984e58123cf425bf2bc50d60fb45b375d363
Hung <duyhung325tt@gmail.com>
2026-01-08 19:43:58 +0700

    them bai 1.6

---
+++ src/bai1_6.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class bai1_6 {
+    static Scanner inp = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Nhap vao nam: ");
+        int year = inp.nextInt();
+        boolean isLeap = false;
+        if (year % 400 == 0 || (year%4 == 0 && year % 100 != 0)) {
+            isLeap = true;
+        }
+        else {
+            isLeap = false;
+        }
+        switch (isLeap ? 2 : 1) {
+            case 1: System.out.println(year + " la nam khong nhuan"); break;
+            case 2: System.out.println(year + " la nam nhuan"); break;
+        }
+    }
+}
