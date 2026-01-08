7a257ca57e3b4058db81e31515216b2d518df827
Hung <duyhung325tt@gmail.com>
2026-01-08 19:13:30 +0700

    chinh sua nhap/xuat bai 1.3

--- src/bai1_3.java
+++ src/bai1_3.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
 public class bai1_3 {
+    static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
         int a = 5;
         int b = 10;
         int max = (a > b) ? a : b;
         System.out.println("So lon hon : "+ max);
+        // nhap so tu ban phim
+        System.out.println("Nhap so a: ");
+        int a1 = input.nextInt();
+        System.out.println("Nhap so b: ");
+        int b1 = input.nextInt();
+        int max1 = (a1 > b1) ? a1 : b1;
+        System.out.println("So lon hon : "+ max1);
     }
 }
