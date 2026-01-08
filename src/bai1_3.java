4dc4b0103ce29b7d710b430d2a540dd71ee471ba
Hung <duyhung325tt@gmail.com>
2026-01-08 19:05:02 +0700

    Them bai 1.3

---
+++ src/bai1_3.java
@@ -0,0 +1,8 @@
+public class bai1_3 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int max = (a > b) ? a : b;
+        System.out.println("So lon hon : "+ max);
+    }
+}
