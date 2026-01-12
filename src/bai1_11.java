5665d79c24ca447b5fca060c10d34b3b28e1a6c1
Hung <duyhung325tt@gmail.com>
2026-01-12 07:54:20 +0700

    hoan thanh bai 1.11

--- src/bai1_11.java
+++ src/bai1_11.java
@@ -1,20 +1,38 @@
 import java.util.Scanner;
 import java.time.LocalDate; // dd-MM-yyyy
 import java.time.format.DateTimeFormatter;
-
-
 public class bai1_11 {
-
-
+    public static boolean checkDate(int day, int month, int year) {
+        try {
+            LocalDate date = LocalDate.of(year, month, day);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+    public static void pre_aftDays(int day , int month , int year){
+        if(checkDate(day, month, year)){
+            LocalDate date = LocalDate.of(year, month, day);
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+            LocalDate previousDate = date.minusDays(1);
+            LocalDate nextDate = date.plusDays(1);
+            System.out.println("Ngay truoc do : " + previousDate.format(formatter));
+            System.out.println("Ngay sau do : " + nextDate.format(formatter));
+        }
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        System.out.print("Nhap vao ngay sinh (dd/MM/yyyy): ");
-        String dobInput = sc.nextLine();
-        LocalDate date = LocalDate.parse(dobInput, dtf);
-        System.out.println("Ngay sinh ban da nhap la: " + date.format(dtf));
-        int day = date.getDayOfMonth();
-        int month = date.getMonthValue();
-        int year = date.getYear();
+        System.out.println("Nhap ngay : ");
+        int day = sc.nextInt();
+        System.out.println("Nhap thang : ");
+        int month = sc.nextInt();
+        System.out.println("Nhap nam : ");
+        int year = sc.nextInt();
+        if(checkDate(day, month, year)) {
+            System.out.println("Ngay vua nhap : " + String.format("%02d-%02d-%04d", day, month, year));
+            pre_aftDays(day, month, year);
+        } else {
+            System.out.println("Ngay khong hop le");
+        }
     }
 }
