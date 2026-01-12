2877611a0b2c32da368b9375b4684093bd750a32
Hung <duyhung325tt@gmail.com>
2026-01-12 08:05:24 +0700

    them bai 1.14

---
+++ src/bai1_14.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.lang.Math;
+public class bai1_14 {
+    public static boolean isPrime(int num) {
+        if (num <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap so nguyen duong n: ");
+        int n = sc.nextInt();
+        if(isPrime(n)) {
+            System.out.println(n + " la so nguyen to");
+        } else {
+            System.out.println(n + " khong phai la so nguyen to");
+        }
+    }
+}
