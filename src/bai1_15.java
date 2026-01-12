df1e8157dbf7138e002987791fe0109b6b98c390
Hung <duyhung325tt@gmail.com>
2026-01-12 17:21:33 +0700

    them bai 1.15

---
+++ src/bai1_15.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class bai1_15 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double sum =0;
+        boolean flagStop = false;
+        do {
+            System.out.println("Nhap so thuc khac 0 :  ");
+            double n = sc.nextDouble();
+            if(n == 0){
+                System.out.println("Da dung chuong trinh");
+                break;
+            }
+            sum += n;
+            System.out.println("Tong cac so vua nhap la: " + sum);
+        }while (true);
+    }
+}
