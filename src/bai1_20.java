973301556e8e4f50527dcb2225aa3fee56265651
Hung <duyhung325tt@gmail.com>
2026-01-13 22:03:02 +0700

    sua lai bai 1.20

--- src/bai1_20.java
+++ src/bai1_20.java
@@ -4,11 +4,27 @@ public class bai1_20 {
         Scanner sc = new Scanner(System.in);
         System.out.println("nhap chieu cao h : ");
         int h = sc.nextInt();
-        for(int i=0;i<h;i++){
-            for(int j=0;j<=i;j++){
-                System.out.print("* ");
+        int w = 2*h -1;
+        int mid = w/2;
+        if(h%2==0){
+            for(int i=0;i<h;i++){
+                for(int j=0;j<=i;j++){
+                    System.out.print("* ");
+                }
+                System.out.println();
             }
-            System.out.println();
+        }
+        else {
+            for(int i=0;i<h;i++){
+                for(int j=0;j<w;j++){
+                    if(j>=mid - i && j<= mid + i){
+                        System.out.print("* ");
+                    } else {
+                        System.out.print("  ");
+                    }
+                }
+                System.out.println();
+            }
         }
     }
 }
