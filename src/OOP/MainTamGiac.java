7bd19c49aeaea7c427bd13c91e7ab5b1113fbdbd
Hung <duyhung325tt@gmail.com>
2026-01-28 21:56:22 +0700

    bai 1.26

---
+++ src/OOP/MainTamGiac.java
@@ -0,0 +1,31 @@
+package OOP;
+import java.util.Scanner;
+
+public class MainTamGiac {
+    public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("tao tam giac !");
+        Diem2D A = new Diem2D(1, 2);
+        Diem2D B = new Diem2D(4, 4);
+        Diem2D C = new Diem2D(3, 9);
+        TamGiac d = new TamGiac(A, B, C);
+        d.Xuat();
+        System.out.println("chu vi tam giac : " + d.TinhChuVi());
+        System.out.println("Dien tich tam giac : " + d.TinhDienTich());
+
+        // kịch bản 1 : góc giữa các vector AB , AC
+        double xAB = B.getX()-A.getX();
+        double yAB = B.getY()-A.getY();
+        double xAC = C.getX()-A.getX();
+        double yAC = C.getY()-A.getY();
+        // tich vo huong
+        double tvh = xAB*xAC  + yAB*yAC;
+        // tich do dai
+        double dAB = Math.sqrt(Math.pow(xAB,2)+Math.pow(yAB,2));
+        double dAC = Math.sqrt(Math.pow(xAC,2)+Math.pow(yAC,2));
+        double tdd = tvh*dAB;
+        //
+        double goc = tdd/tdd;
+        System.out.println("goc giua hai vector AB , AC: "+ goc);
+    }
+}
---
+++ src/OOP/TamGiac.java
@@ -0,0 +1,69 @@
+package OOP;
+import java.util.Scanner;
+
+public class TamGiac {
+    Diem2D dinh1 ,dinh2,dinh3;
+
+    public Diem2D getDinh1() {
+        return dinh1;
+    }
+
+    public void setDinh1(Diem2D dinh1) {
+        this.dinh1 = dinh1;
+    }
+
+    public Diem2D getDinh2() {
+        return dinh2;
+    }
+
+    public void setDinh2(Diem2D dinh2) {
+        this.dinh2 = dinh2;
+    }
+
+    public Diem2D getDinh3() {
+        return dinh3;
+    }
+
+    public void setDinh3(Diem2D dinh3) {
+        this.dinh3 = dinh3;
+    }
+
+    public TamGiac() {
+    }
+
+    public TamGiac(Diem2D d1, Diem2D d2, Diem2D d3) {
+        this.dinh1 = d1;
+        this.dinh2 = d2;
+        this.dinh3 = d3;
+    }
+    public TamGiac(TamGiac tg){
+        this.dinh1 = tg.getDinh1();
+        this.dinh2 = tg.getDinh2();
+        this.dinh3 = tg.getDinh3();
+    }
+    //
+    void Nhap(){
+        dinh1.NhapDiem();
+        dinh2.NhapDiem();
+        dinh3.NhapDiem();
+    }
+    void Xuat(){
+        System.out.println("tam giac co toa do cac diem : ");
+        dinh1.HienThi();
+        dinh2.HienThi();
+        dinh3.HienThi();
+    }
+    double TinhChuVi(){
+        double a = dinh1.KhoangCachDenDiem(dinh2);
+        double b = dinh2.KhoangCachDenDiem(dinh3);
+        double c = dinh3.KhoangCachDenDiem(dinh1);
+        return a+b+c;
+    }
+    double TinhDienTich(){
+        double a = dinh1.KhoangCachDenDiem(dinh2);
+        double b = dinh2.KhoangCachDenDiem(dinh3);
+        double c = dinh3.KhoangCachDenDiem(dinh1);
+        double nuaChuVi = TinhChuVi()/2;
+        return Math.sqrt(nuaChuVi*(nuaChuVi-a)*(nuaChuVi-b)*(nuaChuVi-c));
+    }
+}
